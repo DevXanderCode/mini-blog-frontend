48b59169b03cfafc0c97957562da861026f52e27
Alex Chinedu <nwekealexchinedu@gmail.com>
2023-03-30 00:11:18 +0100

    worked on updating post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -47,6 +47,10 @@ class Feed extends Component {
         case "create":
           this.addPost(data.post);
           break;
+        case "update":
+          this.updatePost(data.post);
+          break;
+
         default:
           return;
       }
@@ -65,6 +69,21 @@ class Feed extends Component {
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
+
+  updatePost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(
+        (p) => p._id === post._id
+      );
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts,
       };
     });
   };
@@ -180,18 +199,18 @@ class Feed extends Component {
           createdAt: resData.post.createdAt,
         };
         this.setState((prevState) => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              (p) => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          }
+          // let updatedPosts = [...prevState.posts];
+          // if (prevState.editPost) {
+          //   const postIndex = prevState.posts.findIndex(
+          //     (p) => p._id === prevState.editPost._id
+          //   );
+          //   updatedPosts[postIndex] = post;
+          // }
           // else if (prevState.posts.length < 2) {
           //   updatedPosts = prevState.posts.concat(post);
           // }
           return {
-            posts: updatedPosts,
+            // posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
