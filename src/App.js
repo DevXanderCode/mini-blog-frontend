69e26b5aee13ed0511961b0f263678bd4623ecde
Alex Chinedu <nwekealexchinedu@gmail.com>
2023-04-12 08:35:39 +0100

    integrated the login graphql endpoint

--- src/App.js
+++ src/App.js
@@ -59,28 +59,42 @@ class App extends Component {
   loginHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch("http://localhost:8080/auth/login", {
+
+    const graphqlQuery = {
+      query: `
+     query {
+       login(email: "${authData.email}", password: "${authData.password}") {
+        token
+        userId
+      }
+     } `,
+    };
+    fetch("http://localhost:8080/graphql", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        email: authData.email,
-        password: authData.password,
-      }),
+      body: JSON.stringify(graphqlQuery),
     })
       .then((res) => {
-        if (res.status === 422) {
-          throw new Error("Validation failed.");
-        }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log("Error!");
-          throw new Error("Could not authenticate you!");
-        }
+        // if (res.status === 422) {
+        //   throw new Error("Validation failed.");
+        // }
+        // if (res.status !== 200 && res.status !== 201) {
+        //   console.log("Error!");
+        //   throw new Error("Could not authenticate you!");
+        // }
         return res.json();
       })
       .then((resData) => {
         console.log(resData);
+        if (resData.errors && resData.errors[0].status === 404) {
+          throw new Error("User not Found.");
+        }
+        if (resData.errors) {
+          console.log("Error!");
+          throw new Error("Could not authenticate you!");
+        }
         this.setState({
           isAuth: true,
           token: resData.token,
